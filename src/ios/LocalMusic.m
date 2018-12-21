cbceb8fd86c3ea4ff4141a2be45bc472e4253da9
noah-lou-chen <chenlounai@zzltop.com>
2018-12-21 18:14:28 +0800

    更新IOS的H5播放路径

--- src/ios/LocalMusic.m
+++ src/ios/LocalMusic.m
@@ -1,5 +1,8 @@
 #import <Cordova/CDV.h>
 #import "LocalMusic.h"
+#import <AVFoundation/AVFoundation.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+#import <AssetsLibrary/AssetsLibrary.h>
 
 @implementation LocalMusic
 
@@ -56,9 +59,11 @@
         NSString *albumTitle = [NSString stringWithFormat:@"%@ ",[song valueForProperty: MPMediaItemPropertyAlbumTitle]];
         NSString *artistTitle = [NSString stringWithFormat:@"%@ ",[song valueForProperty: MPMediaItemPropertyArtist]];
         NSNumber *duration = [NSNumber numberWithLong:[[song valueForKey:MPMediaItemPropertyPlaybackDuration] longValue] * 1000];
-        //链接    注意: 如果后面不调用 absoluteString ,播放会崩溃
+        //调用 absoluteString 转换为字符串
         NSString *url = [[song valueForProperty: MPMediaItemPropertyAssetURL] absoluteString];
-
+ 
+        NSString *dataUrl = [[self convertToMp3:song] absoluteString];
+        
         [songDictionary setObject:songId forKey:@"id"];
         [songDictionary setObject:albumId forKey:@"album_id"];
         [songDictionary setObject:artistId forKey:@"artist_id"];
@@ -67,12 +72,89 @@
         [songDictionary setObject:artistTitle forKey:@"artistName"];
         [songDictionary setObject:duration forKey:@"duration"];
         [songDictionary setObject:url forKey:@"url"];
-
+        [songDictionary setObject:dataUrl forKey:@"data"]; // 可H5标签audio播放的路径
+        
         [allSongs addObject:songDictionary];
     }
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:[allSongs copy]];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
+
+- (NSURL *) convertToMp3: (MPMediaItem*)song{
+    NSURL *url = [song valueForProperty:MPMediaItemPropertyAssetURL];
+    AVURLAsset *songAsset = [AVURLAsset URLAssetWithURL:url options:nil];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSArray *dirs = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES);
+    NSString *documentsDirectoryPath = [dirs objectAtIndex:0];
+    //NSLog(@"compatible presets for songAsset: %@",[AVAssetExportSession exportPresetsCompatibleWithAsset:songAsset]);
+    NSArray *ar = [AVAssetExportSession exportPresetsCompatibleWithAsset: songAsset];
+    //NSLog(@"%@", ar);
+    AVAssetExportSession *exporter = [[AVAssetExportSession alloc] initWithAsset: songAsset presetName:AVAssetExportPresetAppleM4A];
+    //NSLog(@"created exporter. supportedFileTypes: %@", exporter.supportedFileTypes);
+    exporter.outputFileType=@"com.apple.m4a-audio";
+ 
+    NSString *exportFile = [documentsDirectoryPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.m4a",[song valueForProperty:MPMediaEntityPropertyPersistentID]]];
+    NSURL *urlPath= [NSURL fileURLWithPath:exportFile];
+    exporter.outputURL=urlPath;
+    NSLog(@"---------%@",urlPath);
+   
+    [exporter exportAsynchronouslyWithCompletionHandler:^{
+        NSData *data1 = [NSData dataWithContentsOfFile:exportFile];
+        //NSLog(@"==================data1:%@",data1);
+        int exportStatus = exporter.status;
+        switch(exportStatus)
+        {
+            case AVAssetExportSessionStatusFailed: {
+            // log error to text view
+        
+                NSError *exportError = exporter.error;
+                NSLog(@"AVAssetExportSessionStatusFailed: %@", exportError);
+                break;
+            }
+            
+            case AVAssetExportSessionStatusCompleted: {
+                NSLog(@"AVAssetExportSessionStatusCompleted");break;
+            }
+            
+            case AVAssetExportSessionStatusUnknown:
+            {
+                NSLog(@"AVAssetExportSessionStatusUnknown");
+                break;
+                
+            }
+            
+            case AVAssetExportSessionStatusExporting:
+            {
+                NSLog(@"AVAssetExportSessionStatusExporting");
+                break;
+                
+            }
+            
+            caseAVAssetExportSessionStatusCancelled:
+            {
+                NSLog(@"AVAssetExportSessionStatusCancelled");
+                break;
+            }
+                
+            case AVAssetExportSessionStatusWaiting:
+            {
+                NSLog(@"AVAssetExportSessionStatusWaiting");
+                break;
+            }
+                
+            default:
+            {NSLog(@"didn't get export status");break;}
+                
+        }
+        
+    }
+];
+
+    
+    return urlPath;
+}
+    
+ 
 
 -(void)getAlbums:(CDVInvokedUrlCommand *)command{
     NSMutableArray *allAlbums = [[NSMutableArray alloc] init];
