a330da54659aba2275b2c8fad8c237055de866fb
May.lou <lounai@qq.com>
2019-01-10 17:13:20 +0800

    Update LocalMusic.h

--- src/ios/LocalMusic.h
+++ src/ios/LocalMusic.h
@@ -1,22 +1,4 @@
 #import <UIKit/UIKit.h>
 #import <Cordova/CDVPlugin.h>
 #import <MediaPlayer/MediaPlayer.h>
-
-@interface LocalMusic : CDVPlugin
-
-@property (nonatomic, retain) MPMusicPlayerController *musicPlayer;
-@property (strong, atomic) NSString *currendTrackId;
-
--(void)init:(CDVInvokedUrlCommand *)command;
--(void)getMusicList:(CDVInvokedUrlCommand *)command;
--(void)getAlbums:(CDVInvokedUrlCommand *)command;
--(void)getArtists:(CDVInvokedUrlCommand *)command;
-
--(void)playSong:(CDVInvokedUrlCommand *)command;
--(void)pause:(CDVInvokedUrlCommand *)command;
--(void)resume:(CDVInvokedUrlCommand *)command;
--(void)changeSong:(CDVInvokedUrlCommand *)command;
--(void)currentTrackInfo:(CDVInvokedUrlCommand *)command;
-
-
-@end
+ 
